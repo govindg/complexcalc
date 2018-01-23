4872c7d050790ec288753a86ede445877af12660
Govind Pillai <govindg@uw.edu>
2018-01-23 03:31:00 -0800

    Added basic operations to calculator

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,27 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    public func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    public func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    public func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    public func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    public func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    public func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
 }
